36a56588ec230a3b99a1dfd8b7c171e28039d0cb
William Solórzano <wsrequejo@gmail.com>
2021-04-07 00:35:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.image('https://github.com/solor5/directorio_oxigeno/raw/main/img_ox.png', use
 st.title('Directorio de puntos de venta de oxígeno medicinal ')
 st.write('Esta es una iniciativa sin fines de lucro que busca crear un directorio nacional de puntos de venta de oxígeno. Si deseas agregar una empresa o brindar más detalle de los contactos ya existentes, por favor, haz clic [aquí](https://docs.google.com/forms/d/e/1FAIpQLSeE5uEuDBDtwQIhLZPNuSC5KinHWn5Bx3tU9xbu-DzRZxD6jQ/viewform)')
 st.write('Creado por [William Solórzano](https://www.linkedin.com/in/william-solórzano/)')
-st.header('Piura')
+st.header('**Piura**')
 
 df_nan = pd.read_csv('https://raw.githubusercontent.com/solor5/directorio_oxigeno/main/data.csv')
 st.table(df_nan[['Empresa','Teléfonos']])
