d1151aa711b8fe48a417a9a4ceb9e36a424c4e14
William Solórzano <wsrequejo@gmail.com>
2021-04-07 18:23:39 -0500

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -6,10 +6,11 @@ import pandas as pd
 st.image('https://github.com/solor5/directorio_oxigeno/raw/main/img_ox.png', use_column_width=True)
 st.title('Directorio de puntos de venta de oxígeno medicinal ')
 st.write('Esta es una iniciativa sin fines de lucro que busca crear un directorio nacional de puntos de venta de oxígeno. Si deseas agregar una empresa o brindar más detalle de los contactos ya existentes, por favor, haz clic [aquí](https://docs.google.com/forms/d/e/1FAIpQLSeE5uEuDBDtwQIhLZPNuSC5KinHWn5Bx3tU9xbu-DzRZxD6jQ/viewform)')
+st.write('Por el momento, solo se cuenta con información de la ciudad de Piura, cada 24 horas se actualizará con la información proporcionada de otras regiones del Perú.')
 st.write('Creado por [William Solórzano](https://www.linkedin.com/in/william-solórzano/)')
-st.header('Piura')
+st.header('**Piura**')
 
-df_nan = pd.read_excel('https://github.com/solor5/directorio_oxigeno/raw/a58a41722d2ed4494db1871b4f6f777fadae7f92/Excel.xlsx')
+df_nan = pd.read_csv('https://raw.githubusercontent.com/solor5/directorio_oxigeno/main/data.csv')
 st.table(df_nan[['Empresa','Teléfonos']])
 
 df = df_nan.dropna(axis = 0).reset_index(drop = True)
@@ -25,5 +26,6 @@ for i in range(0,df.shape[0],1):
   ).add_to(m)
   count += 1
 
+st.subheader('**Mapa con los puntos de venta**')
 # call to render Folium map in Streamlit
 folium_static(m)
