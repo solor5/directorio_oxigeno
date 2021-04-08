b2e75aa41f397d04d77050db875b6110c92db50f
William Solórzano <wsrequejo@gmail.com>
2021-04-07 19:33:01 -0500

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -36,8 +36,8 @@ opciones = ['Amazonas', 'Ancash', 'Apurimac', 'Arequipa', 'Ayacucho', 'Cajamarca
 
 st.header('**Formulario**')
 nombre = st.text_input('Nombre de la empresa o contacto')
-telefono1 = st.number_input('Teléfono 1', format="{:,.d}")
-telefono2 = st.number_input('Teléfono 2', format="{:,.f}")
+telefono1 = st.number_input('Teléfono 1', format="{i}")
+telefono2 = st.number_input('Teléfono 2', format="{i}")
 departamento = st.selectbox('Departamento', opciones)
 ciudad = st.text_input('Nombre del distrito o ciudad')
 direccion = st.text_input('Dirección de la empresa, puede adjuntar el enlace de Google Maps')
